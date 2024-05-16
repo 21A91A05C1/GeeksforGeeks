b0b89ba21f04366e8a2290a7cef2f337bdc33a59
21A91A05C1 <97424514+21A91A05C1@users.noreply.github.com>
2024-05-16 21:25:05 +0530

    Updated Solution - GfG to GitHub

--- Medium/Detect cycle in an undirected graph/detect-cycle-in-an-undirected-graph.cpp
+++ Medium/Detect cycle in an undirected graph/detect-cycle-in-an-undirected-graph.cpp
@@ -6,33 +6,36 @@ using namespace std;
 class Solution {
   public:
     // Function to detect cycle in an undirected graph.
-    bool ans=false;
-    void DFS(vector<bool>&vis,vector<int>adj[],int i,int par)
+    int dfs(vector<int>adj[],vector<int>&vis,int i,int par)
     {
-        vis[i]=true;
+        vis[i]=1;
         for(auto it:adj[i])
         {
             if(!vis[it]){
-                DFS(vis,adj,it,i);
+                if(dfs(adj,vis,it,i)) return 1;
             }
             else{
                 if(it!=par){
-                    ans=true;
-                    return ;
+                    return 1;
                 }
             }
         }
+        return 0;
     }
-    bool isCycle(int V,vector<int>adj[]) {
-        vector<bool>vis(V,false);
+    bool isCycle(int V, vector<int> adj[]) {
+        // Code here
+        vector<int>vis(V+1,0);
         for(int i=0;i<V;i++)
         {
+            int k=0;
             if(!vis[i]){
-                DFS(vis,adj,i,-1);
+                k=dfs(adj,vis,i,-1);
+                if(k==1) return 1;
             }
         }
-        return ans;
+        return 0;
     }
+    // 1->2->3
 };
 
 //{ Driver Code Starts.
